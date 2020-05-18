f9fbf99ed3967ebbf6dca91fbf7f9e5933392d70
NoeL Blanco <noelblanco@NoeLs-MBP.fios-router.home>
2020-05-17 20:59:01 -0400

    Updates

---
+++ precache-manifest.31f9e97b398caf5bf2e9333287d2a9ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cc963a5a2f12dbc024d2769d973b38",
+    "url": "/robot-friends-project/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robot-friends-project/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fd6565244e0400ecc8c4",
+    "url": "/robot-friends-project/static/css/main.4f56ccb5.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robot-friends-project/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robot-friends-project/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6565244e0400ecc8c4",
+    "url": "/robot-friends-project/static/js/main.b5ad1525.chunk.js"
+  },
+  {
+    "revision": "fdce6c3e672de4fc2093",
+    "url": "/robot-friends-project/static/js/runtime-main.91ac5ae2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friends-project/static/media/SEGA.12a3db8b.woff"
+  }
+]);
